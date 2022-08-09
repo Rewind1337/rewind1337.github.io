d4161a36b93faf3a7965e3a7a4e5d10b687b46fc
Nicolai Krechel <Rewind1337@hotmail.com>
2022-08-09 19:44:43 +0200

    styles

--- js/main.js
+++ js/main.js
@@ -58,6 +58,10 @@ $(document).ready(function () {
     e.stopPropagation();
   })
 
+  $("a").click(function (e) {
+    e.stopPropagation();
+  })
+
   $(document).resize(function () {
     updateScrolls();
   })
