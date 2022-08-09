f678580f7804d883abba47204bb2c8e7d3a7faa1
Nicolai Krechel <42512084+Rewind1337@users.noreply.github.com>
2022-08-09 13:49:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,14 +5,30 @@ $(document).ready(function () {
   viewportHeight = window.innerHeight;
   viewportWidth = window.innerWidth;
 
-  updateScrolls();
-
-
   $("#sec-a").delay(100).animate({"opacity": 1}, 500);
   $("#sec-b").delay(200).animate({"opacity": 1}, 500);
   $("#sec-c").delay(300).animate({"opacity": 1}, 500);
   $("#sec-d").delay(400).animate({"opacity": 1}, 500);
   $("#sec-e").delay(500).animate({"opacity": 1}, 500);
+
+  $("#piz-a").delay(200).animate({"opacity": 1}, 500);
+  $("#piz-b").delay(300).animate({"opacity": 1}, 500);
+  $("#piz-c").delay(400).animate({"opacity": 1}, 500);
+  $("#piz-d").delay(500).animate({"opacity": 1}, 500);
+  $("#piz-e").delay(600).animate({"opacity": 1}, 500);
+
+  setTimeout(function () {typeOut("Rewind's Github Index", "#title-a", 100)}, 300)
+  setTimeout(function () {typeOut("Game Corner", "#title-b", 120)}, 400)
+  setTimeout(function () {typeOut("Tool Box", "#title-c", 130)}, 500)
+  setTimeout(function () {typeOut("Contact Me", "#title-d", 140)}, 600)
+  setTimeout(function () {typeOut("The End", "#title-e", 150)}, 700)
+
+  setTimeout(function () {typeOut("- about me -", "#subtitle-a", 80)}, 500)
+  setTimeout(function () {typeOut("- all kinds of games -", "#subtitle-b", 80)}, 600)
+  setTimeout(function () {typeOut("- tools and things -", "#subtitle-c", 80)}, 700)
+  setTimeout(function () {typeOut("- jolly cooperation -", "#subtitle-d", 80)}, 800)
+  setTimeout(function () {typeOut("- social links and more -", "#subtitle-e", 80)}, 900)
+  
   $("*").animate({"opacity": 1}, 200);
 
   $("body").scroll(function () {updateScrolls();})
@@ -37,19 +53,41 @@ $(document).ready(function () {
     e.stopPropagation();
   })
 
+  $("input").click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  })
+
   $(document).resize(function () {updateScrolls();})
+
+  updateScrolls();
 })
 
 function updateScrolls () {
   let b = $("#sec-b")[0].getBoundingClientRect().top
-  $("#piz-b").css({"height": Math.min(b, viewportHeight) + "px"})
+  $("#piz-b").css({"height": Math.min(b + 5, viewportHeight) + "px"})
 
   let c = $("#sec-c")[0].getBoundingClientRect().top
-  $("#piz-c").css({"height": Math.min(c, viewportHeight) + "px"})
+  $("#piz-c").css({"height": Math.min(c + 5, viewportHeight) + "px"})
 
   let d = $("#sec-d")[0].getBoundingClientRect().top
-  $("#piz-d").css({"height": Math.min(d, viewportHeight) + "px"})
+  $("#piz-d").css({"height": Math.min(d + 5, viewportHeight) + "px"})
 
   let e = $("#sec-e")[0].getBoundingClientRect().top
-  $("#piz-e").css({"height": Math.min(e, viewportHeight) + "px"})
+  $("#piz-e").css({"height": Math.min(e + 5, viewportHeight) + "px"})
+}
+
+function typeOut(word, destination, speed) {
+  $(destination).html("");
+  for (let i = 0; i < word.length; i++) {
+    setTimeout(function () {
+      let html = $(destination).html();
+      $(destination).html(html.substring(0, html.length - 1) + word.charAt(i) + "_");
+    }, i * speed)
+  }
+
+  setTimeout(function () {
+    let html = $(destination).html();
+    $(destination).html(html.substring(0, html.length - 1));
+  }, word.length * speed)
 }
