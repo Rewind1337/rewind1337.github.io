7f2638fbb4f621aa00be3755dae9001b45532317
Nicolai Krechel <Rewind1337@hotmail.com>
2022-08-27 17:32:52 +0200

    trying to optimize

--- sandbox/js/main.js
+++ sandbox/js/main.js
@@ -1,26 +1,48 @@
+// p5 canvas
 let canvas;
 
+// pixelarray
 let PIXELS;
 
+// tick timer
 let global_timer;
 
+// selection from ui
 let selectedType = 0;
 
+// ticks per second
 const TPS = 30;
 
-const PIXEL_SIZE = 10;
+// pixel size
+let PIXEL_SIZE = 20;
 
+// amount of pixels per side
+let GRID_WIDTH = 40;
+let GRID_HEIGHT = 20;
+
+// particles - special
 const AIR = 0;
-const WALL = 1;
+
+// particles - powders
 const SAND = 10;
 const STONE = 11;
+
+// particles - liquids
 const WATER = 100;
+const LAVA = 101;
+
+// particles - gases
+const STEAM = 200;
+
+// particles - solids
+const WALL = 1;
 
 class Pixel {
 	constructor (type, x, y) {
 		this.x = x;
 		this.y = y;
 
+		this.timeAlive = 0;
 		this.setType(type);
 
 		this.doTick = true;
@@ -50,6 +72,12 @@ class Pixel {
 			case WATER:
 				this.fill = color(200, 100, 65 + random(0, 15));
 			break;
+			case LAVA:
+				this.fill = color(random(0, 10), 100, 20 + random(0, 15));
+			break;
+			case STEAM:
+				this.fill = color(0, 0, 70 + random(0, this.timeAlive));
+			break;
 			case WALL:
 				this.fill = color(0, 0, 0 + random(0, 15));
 			break;
@@ -62,6 +90,8 @@ class Pixel {
 
 	tick () {
 		if (this.type === AIR || this.type === WALL) return;
+
+		this.timeAlive ++;
 
 		if (!this.doTick) {
 			this.doTick = true;
@@ -88,6 +118,31 @@ class Pixel {
 			case WALL:
 			case AIR:
 
+			break;
+			case STEAM:
+				this.fill = color(0, 0, 70 + random(0, 20));
+				if (up.type === AIR) {
+					this.swap(up);
+					return;
+				}
+				if (up.type !== AIR) {
+					if (true) { // move or not?
+						if (Math.random() >= 0.5) {
+							if (left.type === AIR) {
+								this.swap(left);
+								return;
+							}
+						} else {
+							if (right.type === AIR) {
+								this.swap(right);
+								return;
+							}
+						}
+					}
+				}
+
+				// considered stable, no calculation
+				this.doTick = false;
 			break;
 			case SAND:
 				if (down.type === AIR || down.type === WATER) {
@@ -124,14 +179,47 @@ class Pixel {
 				this.doTick = false;
 			break;
 			case WATER:
-				if (down.type === AIR) {
+				if (down.type === AIR || down.type === STEAM) {
 					this.swap(down);
 					return;
 				}
 				if (down.type !== AIR) {
 					if (true) { // move or not?
 						if (Math.random() >= 0.5) {
-							if (left.type == AIR) {
+							if (left.type === AIR || left.type === STEAM) {
+								this.swap(left);
+								return;
+							}
+						} else {
+							if (right.type === AIR || right.type === STEAM) {
+								this.swap(right);
+								return;
+							}
+						}
+					}
+				}
+
+				// considered stable, no calculation
+				this.doTick = false;
+			break;
+			case LAVA:
+				if (down.type === AIR || down.type === STEAM) {
+					this.swap(down);
+					return;
+				}
+
+				if (down.type === WATER) {
+					down.setType(STEAM);
+					if (Math.random() >= 0.5) {
+						this.setType(STEAM);
+					}
+					return;
+				}
+
+				if (down.type !== AIR) {
+					if (Math.random() >= 0.5) { // move or not?
+						if (Math.random() >= 0.5) {
+							if (left.type === AIR) {
 								this.swap(left);
 								return;
 							}
@@ -151,19 +239,24 @@ class Pixel {
 	}
 
 	draw () {
-		stroke(0, 0, 0, 0.1);
-		fill(this.fill);
-		rect(this.x * PIXEL_SIZE, this.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+		drawingContext.fillStyle = this.fill.toString(RGB);
+		drawingContext.fillRect(this.x * PIXEL_SIZE, this.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
 	}
 }
 
 function importB64 (string) {
 	clearInterval(global_timer);
 
-	let _PIXELS = JSON.parse(atob(string));
+	let importJSON = JSON.parse(atob(string));
 
-	for (let x = 0; x < _PIXELS.length; x++) {
-		for (let y = 0; y < _PIXELS[x].length; y++) {
+	GRID_WIDTH = importJSON.GRID_WIDTH;
+	GRID_HEIGHT = importJSON.GRID_HEIGHT;
+	PIXEL_SIZE = importJSON.PIXEL_SIZE;
+
+	let _PIXELS = importJSON.PIXELS;
+
+	for (let x = 0; x < GRID_WIDTH; x++) {
+		for (let y = 0; y < GRID_HEIGHT; y++) {
 			PIXELS[x][y].setType(_PIXELS[x][y].type);
 		}
 	}
@@ -172,17 +265,30 @@ function importB64 (string) {
 }
 
 function exportB64 () {
+	// setup json for export
+	let exportJSON = {
+		PIXELS: undefined,
+		PIXEL_SIZE: PIXEL_SIZE,
+		GRID_WIDTH: GRID_WIDTH,
+		GRID_HEIGHT: GRID_HEIGHT,
+	}
+
+	// setup array of pixels
 	let exportArray = [];
 	for (let x = 0; x < PIXELS.length; x++) {
 		exportArray.push([]);
-		for (let y = 0; y < PIXELS[x].length; y++) {
+		for (let y = 0; y < PIXELS[0].length; y++) {
 			exportArray[x].push([]);
 			let P = PIXELS[x][y];
 			let clean = {x: P.x, y: P.y, type: P.type};
 			exportArray[x][y] = clean;
 		}
 	}
-	return btoa(JSON.stringify(exportArray));
+
+	// add pixels into json
+	exportJSON.PIXELS = exportArray;
+
+	return btoa(JSON.stringify(exportJSON));
 }
 
 function preload () {}
@@ -199,28 +305,28 @@ function setup () {
 	smooth();
 	angleMode(DEGREES);
 	frameRate(TPS);
-	setupSandbox(800, 400);
+	setupSandbox();
 	bindToolbar();
 	startTickClock(30);
 }
 
 function startTickClock () {
 	global_timer = setInterval(function () {
-		for (let x = 0; x < width/PIXEL_SIZE; x++) {
-			for (let y = 0; y < height/PIXEL_SIZE; y++) {
+		for (let x = 0; x < PIXELS.length; x++) {
+			for (let y = 0; y < PIXELS[0].length; y++) {
 				PIXELS[x][y].tick();
 			}
 		}
 	}, 1000 / TPS)
 }
 
-function setupSandbox(width, height) {
-	resizeCanvas(width, height);
+function setupSandbox() {
+	resizeCanvas(GRID_WIDTH * PIXEL_SIZE, GRID_HEIGHT * PIXEL_SIZE);
 
 	PIXELS = [];
-	for (let x = 0; x < width/PIXEL_SIZE; x++) {
+	for (let x = 0; x < GRID_WIDTH; x++) {
 		PIXELS.push([]);
-		for (let y = 0; y < height/PIXEL_SIZE; y++) {
+		for (let y = 0; y < GRID_HEIGHT; y++) {
 			PIXELS[x].push([]);
 			PIXELS[x][y] = new Pixel(0, x, y);
 		}
