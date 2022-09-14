34ba9bae5f5371970db809a2189bd758ad2ffd65
Nicolai Krechel <Rewind1337@hotmail.com>
2022-09-14 22:49:29 +0200

    compression

--- sandbox/js/main.js
+++ sandbox/js/main.js
@@ -626,6 +626,11 @@ function importGridState (string) {
 	clearInterval(globalTimer);
 
 	let importJSON = JSON.parse(string);
+	let isCompressed = (importJSON.T !== undefined ? true : false);
+
+	if (isCompressed) {
+		importJSON = uncompress(importJSON);
+	}
 
 	GRID_WIDTH = importJSON.GRID_WIDTH;
 	GRID_HEIGHT = importJSON.GRID_HEIGHT;
@@ -667,14 +672,53 @@ function exportGridState () {
 
 	// add pixels into json
 	exportJSON.PIXELS = exportArray;
+	let uncompressedExportJSON = copy(exportJSON);
+	let compressedExportJSON = compress(exportJSON);
 
-	return JSON.stringify(exportJSON);
+	if (JSON.stringify(compressedExportJSON).length < JSON.stringify(uncompressedExportJSON).length) {
+		return JSON.stringify(compressedExportJSON);
+	} else {
+		return JSON.stringify(uncompressedExportJSON);
+	}
 }
 
+function uncompress (compressedJsonObject) {
+	// '{"P":[[0,1,1,1,1],[0,2,1,1,1],[0,2,3,3,3],[0,2,3,3,3],[0,2,3,3,3]],"PS":40,"GW":5,"GH":5,T:[{a:"0",b:"[0,0]"},{a:"1",b:"[501,0]"},{a:"2",b:"[300,0]"},{a:"3",b:"[100,0]"}]}';
+	// turns into
+	// '{"PIXELS":[[[0,0],[300,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]]],"PIXEL_SIZE":40,"GRID_WIDTH":5,"GRID_HEIGHT":5}';
+	
+	let pixels = compressedJsonObject.P;
+	let tokens = compressedJsonObject.T;
+	for (let row = 0; row < pixels.length; row++) {
+		for (let col = 0; col < pixels[row].length; col++) {
+			let particle = pixels[row][col];
+
+			tokencheck:
+			for (let t in tokens) {
+				if (tokens[t].a == particle) {
+					particle = tokens[t].b;
+					pixels[row][col] = particle;
+				}
+			}
+		}
+	}
+
+	let uncompressedJsonObject = {
+		PIXELS: pixels,
+		PIXEL_SIZE: compressedJsonObject.PS,
+		GRID_WIDTH: compressedJsonObject.GW,
+		GRID_HEIGHT: compressedJsonObject.GH
+	}
+
+	return uncompressedJsonObject;
+}
 
 function compress (jsonObject) {
-	// first pass, tokenize
-	let pixels = jsonObject.PIXELS;
+	// '{"PIXELS":[[[0,0],[300,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]]],"PIXEL_SIZE":40,"GRID_WIDTH":5,"GRID_HEIGHT":5}';
+	// turns into
+	// '{"P":[[0,1,1,1,1],[0,2,1,1,1],[0,2,3,3,3],[0,2,3,3,3],[0,2,3,3,3]],"PS":40,"GW":5,"GH":5,T:[{a:"0",b:"[0,0]"},{a:"1",b:"[501,0]"},{a:"2",b:"[300,0]"},{a:"3",b:"[100,0]"}]}';
+
+	let pixels = copy(jsonObject.PIXELS);
 	let tokens = [];
 	let tokenN = 0;
 	for (let row = 0; row < pixels.length; row++) {
@@ -682,35 +726,42 @@ function compress (jsonObject) {
 			let particle = pixels[row][col];
 			let token = {a: tokenN, b: particle};
 			let unique = true;
+
+			uniquecheck:
 			for (let t in tokens) {
 				if (tokens[t].b[0] == particle[0] && tokens[t].b[1] == particle[1]) {
 					unique = false;
+					break uniquecheck;
 				}
 			}
+
 			if (unique) {
 				tokens.push(token);
 				tokenN++;
 			}
 
-			//  if particle in tokens.b
-			// 		replace pixels[row][col] with token.a
+			for (let t in tokens) {
+				if (tokens[t].b[0] == particle[0] && tokens[t].b[1] == particle[1]) {
+					pixels[row][col] = tokens[t].a;
+				}
+			}
 		}
 	}
 
-	console.log(tokens); // works
+	let compressedJsonObject = {
+		P: pixels,
+		PS: jsonObject.PIXEL_SIZE,
+		GW: jsonObject.GRID_WIDTH,
+		GH: jsonObject.GRID_HEIGHT,
+		T: tokens,
+	}
 
-	// add tokens to jsonObject
-	// replace jsonObject.PIXELS with pixels
-	// shorten variable names
+	return compressedJsonObject;
 	
-	let before = '{"PIXELS":[[[0,0],[300,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[300,0],[300,0],[300,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]],[[0,0],[500,0],[310,0],[310,0],[310,0]]],"PIXEL_SIZE":40,"GRID_WIDTH":5,"GRID_HEIGHT":5}';
-	// turns into
-	let after = '{"P":[[0,1,1,1,1],[0,2,1,1,1],[0,2,3,3,3],[0,2,3,3,3],[0,2,3,3,3]],"PS":40,"GW":5,"GH":5,_T:[{a:"0",b:"[0,0]"},{a:"1",b:"[501,0]"},{a:"2",b:"[300,0]"},{a:"3",b:"[100,0]"}]}';
-	// compression rate of a 5x5 mixed pixel grid = 30% smaller
-	// compression rate of a 5x5 empty(air) pixel grid = 35% smaller
-	// compression rate of a 25x25 mixed pixel grid = 70% smaller
-	// compression rate of a 25x25 empty(air) pixel grid = 64% smaller
-	// didnt test for bigger grids yet
+	// compression rate of a 5x5 mixed pixel grid, no fixed particles = 30% smaller
+	// compression rate of a 5x5 empty(air) pixel grid, no fixed particles = 35% smaller
+	// compression rate of a 25x25 mixed pixel grid, no fixed particles = 70% smaller
+	// compression rate of a 25x25 empty(air) pixel grid, no fixed particles = 64% smaller
 }
 
 function preload () {}
@@ -855,7 +906,7 @@ function stopTooltipTimer () {
 		$("#tooltip").css({"opacity": 0, "top": "-10000px", "left": "-10000px", "display": "none"});
 	}
 }
-
+ 
 function bindToolbar () {
 	$(".particle:not(.particle-wip)").click(function () {
 		let type = eval($(this).attr("data-type"));
@@ -1080,3 +1131,5 @@ function keyReleased () {}
 function windowResized() {}
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+const copy = (obj) => JSON.parse(JSON.stringify(obj));
